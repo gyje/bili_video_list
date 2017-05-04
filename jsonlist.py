06e401a45dcf65f5b75696765dbe3d75de4efc2b
YingJie <gyj37191@gmail.com>
2017-05-04 19:14:06 +0800

    veersion 3.0
    修正错误

--- jsonlist.py
+++ jsonlist.py
@@ -13,11 +13,11 @@ headers={
 'Connection':'keep-alive'
 }
 fob=open("video_info.txt","w+",encoding="utf-8")
-pages_num=int(input("空间视频页数量"))+1
-space_aid=input("空间id")
+pages_num=int(input("空间视频页数量\n"))+1
+space_aid=input("空间id\n")
 format = '%Y-%m-%d %H:%M:%S'
 for i in range(1,pages_num):
-	url="http://space.bilibili.com/ajax/member/getSubmitVideos?mid="+space_url+"&pagesize=30&tid=0&page="+str(i)+"&keyword=&order=senddate&_=1493786538801"
+	url="http://space.bilibili.com/ajax/member/getSubmitVideos?mid="+space_aid+"&pagesize=30&tid=0&page="+str(i)+"&keyword=&order=senddate&_=1493786538801"
 	html_json=requests.get(url,headers=headers)
 	video_json_list=html_json.json()
 	for l in range(30):
@@ -30,5 +30,6 @@ for i in range(1,pages_num):
 		fob.write("播放数:"+str(video_json_list["data"]["vlist"][l]["play"])+"\n")
 		fob.write("收藏:"+str(video_json_list["data"]["vlist"][l]["favorites"])+"\n")
 		fob.write("评论数:"+str(video_json_list["data"]["vlist"][l]["video_review"])+"\n")
-		fob.write("视频时长:"+video_json_list["data"]["vlist"][l]["length"]+"\n\n\n\n\n\n")
+		fob.write("视频时长:"+video_json_list["data"]["vlist"][l]["length"]+"\n\n\n\n")
 	time.sleep(2)
+fob.close()
