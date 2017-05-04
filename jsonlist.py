d74e4e480034dc392918de70425508de70176c39
YingJie <gyj37191@gmail.com>
2017-05-04 19:08:06 +0800

    version 2.0
    增加通用性

--- jsonlist.py
+++ jsonlist.py
@@ -12,37 +12,23 @@ headers={
 'Cookie': 'buvid3=97822C70-F77E-48FE-BCFD-492B2AB5949428064infoc; fts=1493786212',
 'Connection':'keep-alive'
 }
-fob=open("info.txt","w+",encoding="utf-8")
-for i in range(1,2):
-	url="http://space.bilibili.com/ajax/member/getSubmitVideos?mid=20423027&pagesize=30&tid=0&page="+str(i)+"&keyword=&order=senddate&_=1493786538801"
+fob=open("video_info.txt","w+",encoding="utf-8")
+pages_num=int(input("空间视频页数量"))+1
+space_aid=input("空间id")
+format = '%Y-%m-%d %H:%M:%S'
+for i in range(1,pages_num):
+	url="http://space.bilibili.com/ajax/member/getSubmitVideos?mid="+space_url+"&pagesize=30&tid=0&page="+str(i)+"&keyword=&order=senddate&_=1493786538801"
 	html_json=requests.get(url,headers=headers)
 	video_json_list=html_json.json()
 	for l in range(30):
 		fob.write("标题:"+video_json_list["data"]["vlist"][l]["title"]+"\n")
 		fob.write("播放地址:http://www.bilibili.com/video/av"+str(video_json_list["data"]["vlist"][l]["aid"])+"\n")
 		fob.write("描述:"+video_json_list["data"]["vlist"][l]["description"]+"\n")
-		format = '%Y-%m-%d %H:%M:%S'
 		value = time.localtime(int(video_json_list["data"]["vlist"][l]["created"]))
 		dt = time.strftime(format, value)
 		fob.write("创建时间:"+dt+"\n")
 		fob.write("播放数:"+str(video_json_list["data"]["vlist"][l]["play"])+"\n")
-		# fob.write("硬币数??:"+str(video_json_list["data"]["vlist"][l]["review"])+"\n")
 		fob.write("收藏:"+str(video_json_list["data"]["vlist"][l]["favorites"])+"\n")
 		fob.write("评论数:"+str(video_json_list["data"]["vlist"][l]["video_review"])+"\n")
 		fob.write("视频时长:"+video_json_list["data"]["vlist"][l]["length"]+"\n\n\n\n\n\n")
 	time.sleep(2)
-		
-
-#html_json=requests.get(url,headers=headers)
-#html_json.encoding="utf-8"
-#video_json_list=html_json.json()
-#print (type(video_json_list))
-# print ()
-# print ()
-# print ()
-# print ()
-# print ()
-# print ()
-# print ()
-# print ()
-# print ()
